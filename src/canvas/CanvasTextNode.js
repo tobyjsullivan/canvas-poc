e1758de6407550da57a354f7447b00c506129320
Toby Sullivan <tobyjsullivan@gmail.com>
2022-10-10 22:05:41 -0700

    Use pixi to speed up line wrapping

--- src/canvas/CanvasTextNode.js
+++ src/canvas/CanvasTextNode.js
@@ -1,76 +1,5 @@
-import TextRenderNode from "../render/TextRenderNode.js";
+import TextRenderNode, { wordWrapLines } from "../render/TextRenderNode.js";
 import CanvasNode from "./CanvasNode.js";
-
-function measureTextWidth(words, textProps) {
-  const content = words.join(" ");
-  const node = new TextRenderNode({ ...textProps, content });
-  const {
-    graphic,
-    boundingBox: { width },
-  } = node.render();
-  graphic.destroy();
-  return width;
-}
-
-/**
- *
- * @param {*} words
- * @param {*} maxWidth
- * @param {*} textProps
- * @param {*} rangeStart
- * @param {*} rangeEnd
- *
- * @returns {number} Index of last word that fits, or -1 if no words fit.
- */
-function findSubstringToFit(
-  words,
-  maxWidth,
-  textProps,
-  rangeStart = 0,
-  // End index non-inclusive
-  rangeEnd = words.length
-) {
-  if (rangeStart >= rangeEnd) {
-    // Empty range
-    return -1;
-  }
-
-  if (rangeStart === rangeEnd - 1) {
-    // Single word in range. Measure length of words up to then.
-    const width = measureTextWidth(words.slice(0, rangeStart + 1), textProps);
-    if (width <= maxWidth) {
-      return rangeStart;
-    } else {
-      // No match
-      return -1;
-    }
-  }
-
-  // Split range in half and measure each
-  const mid = Math.ceil((rangeEnd - rangeStart) / 2) + rangeStart;
-  const firstHalf = words.slice(0, mid);
-
-  if (measureTextWidth(firstHalf, textProps) > maxWidth) {
-    // Recursively search first half
-    return findSubstringToFit(words, maxWidth, textProps, rangeStart, mid);
-  } else {
-    // Search the second half
-    const result = findSubstringToFit(
-      words,
-      maxWidth,
-      textProps,
-      mid,
-      rangeEnd
-    );
-
-    if (result === -1) {
-      // The first half fit exactly.
-      return mid - 1;
-    }
-
-    return result;
-  }
-}
 
 class CanvasTextNode extends CanvasNode {
   constructor(props) {
@@ -82,47 +11,23 @@ class CanvasTextNode extends CanvasNode {
     const { parent } = context;
     const { width: parentWidth } = parent;
 
+    const renderProps = { x, y };
+
     if (parentWidth === undefined) {
       // Put everything in
-      const node = new TextRenderNode({ x, y, content });
+      const node = new TextRenderNode({ ...renderProps, content });
       return { nodes: [node] };
     }
 
     console.time("render-text");
-    let words = content.trim().split(/\s+/g);
-    let startIdx = 0;
-    let endIdx = words.length;
-    const lines = [];
-    while (words.length > 0) {
-      const nextLineEnd = findSubstringToFit(
-        words,
-        parentWidth,
-        {},
-        startIdx,
-        endIdx
-      );
-
-      if (nextLineEnd === -1) {
-        // No words fit. Consume one with expectation that it will overflow.
-        const [word, ...rest] = words;
-        lines.push(word);
-        words = rest;
-        continue;
-      }
-
-      const line = words.slice(0, nextLineEnd + 1).join(" ");
-      console.timeLog("render-text", `line:`, line);
-      lines.push(line);
-      words = words.slice(nextLineEnd + 1);
-    }
-
+    const lines = wordWrapLines(content.trim(), renderProps, parentWidth);
     const nodes = [];
     for (const line of lines) {
-      const node = new TextRenderNode({ x, y, content: line });
+      const node = new TextRenderNode({ ...renderProps, content: line });
       nodes.push(node);
     }
-
     console.timeEnd("render-text");
+
     return { nodes };
   }
 }
--- src/render/TextRenderNode.js
+++ src/render/TextRenderNode.js
@@ -1,5 +1,30 @@
 import RenderNode from "./RenderNode.js";
 
+function getTextStyle(props) {
+  // TODO: Support font styling props
+  const fontFamily = "Ariel";
+  const fontSize = 24;
+  const fillColor = PIXI.utils.string2hex("#666666");
+  const textAlign = "left";
+  return {
+    fontFamily: fontFamily,
+    fontSize: fontSize,
+    fill: fillColor,
+    align: textAlign,
+  };
+}
+
+export function wordWrapLines(content, textProps, maxWidth) {
+  const styleOpts = {
+    ...getTextStyle(textProps),
+    wordWrap: true,
+    wordWrapWidth: maxWidth,
+  };
+  const style = new PIXI.TextStyle(styleOpts);
+  const { lines } = PIXI.TextMetrics.measureText(content, style);
+  return lines;
+}
+
 class TextRenderNode extends RenderNode {
   constructor(props) {
     super(props);
@@ -7,23 +32,9 @@ class TextRenderNode extends RenderNode {
 
   render() {
     const { x, y, content } = this.props;
-    // TODO: Support font styling props
-    const fontFamily = "Ariel";
-    const fontSize = 24;
-    const fillColor = PIXI.utils.string2hex("#666666");
-    const textAlign = "left";
 
-    // const graphic = new PIXI.Graphic();
-
-    // graphic.beginFill(fillColor);
-    // graphic.
-
-    const text = new PIXI.Text(content, {
-      fontFamily: fontFamily,
-      fontSize: fontSize,
-      fill: fillColor,
-      align: textAlign,
-    });
+    const styleOpts = getTextStyle(this.props);
+    const text = new PIXI.Text(content, styleOpts);
     text.x = x;
     text.y = y;
 
